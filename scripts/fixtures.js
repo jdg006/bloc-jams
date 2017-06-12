aaffadadb5b015de62efd0c67d6785b3c24bcb94
jdg006 <jdg006@lvc.edu>
2017-06-12 12:10:33 -0400

    checkpoint-19-complete

---
+++ scripts/fixtures.js
@@ -0,0 +1,43 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+ var albumMy = {
+     title: 'The title',
+     artist: 'The artist',
+     label: 'The label',
+     year: '1643',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'The first song', duration: '1:00' },
+         { title: 'The second song', duration: '2:00' },
+         { title: 'The third song', duration: '3:00'},
+         { title: 'The fourth song', duration: '4:00' },
+         { title: 'The fifth song', duration: '5:00'}
+     ]
+ };
